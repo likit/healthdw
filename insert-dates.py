e5b5be177361791554ba7dc9f98c79c20d292f0c
Likit Preeyanon <likit.pre@mahidol.edu>
2017-07-01 17:39:32 +0700

    updated script according to new dates table

--- insert-dates.py
+++ insert-dates.py
@@ -8,7 +8,7 @@ year = int(sys.argv[1])
 
 # connect to the database
 print('Connecting to the database..')
-engine = create_engine('postgresql+psycopg2://likit:password@localhost/healthdw_dev')
+engine = create_engine('postgresql+psycopg2://likit:password@localhost/healthdw_new')
 
 connect = engine.connect()
 
@@ -28,7 +28,7 @@ for month in range(1,13):
             if connect.execute(select([date_table]).where(
                 and_(
                 date_table.c.day==d.day,
-                date_table.c.month==d.month,
+                date_table.c.month_no==d.month,
                 date_table.c.gregorian_year==d.year)
                 )).fetchone():
                 continue
@@ -36,11 +36,23 @@ for month in range(1,13):
                 if d.month != month:
                     continue
                 from_newyear = d - newyear_date
+                if d.month < 9:
+                    fiscal_year = d.year - 1
+                else:
+                    fiscal_year = d.year
+                date_id = str(d.year) + '{0:02d}'.format(d.month) + '{0:02d}'.format(d.day)
+                date_id = int(date_id)
                 ins = date_table.insert().values(
+                        date_id=date_id,
                         day=d.day,
-                        month=d.month,
+                        month_no=d.month,
+                        month=d.strftime('%B'),
                         quarter=quarters[d.month],
                         gregorian_year=d.year,
-                        day_no=from_newyear.days + 1
+                        day_of_year=from_newyear.days + 1,
+                        day_of_week=d.weekday(),
+                        buddhist_year=d.year + 540,
+                        fiscal_year=fiscal_year,
+                        weekday=d.strftime('%A')
                         )
                 result = connect.execute(ins)
