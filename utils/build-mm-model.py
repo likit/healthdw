d364bf1aa036dd9fe84bd18216772b49dee01886
Likit Preeyanon <likit.pre@mahidol.edu>
2018-02-26 05:26:17 +0700

    added and moved build-mm-model.py to utils

--- utils/build-mm-model.py
+++ utils/build-mm-model.py
@@ -5,12 +5,15 @@ from sqlalchemy.sql import and_
 import numpy as np
 from collections import defaultdict
 
-# engine = create_engine('postgresql+psycopg2://likit@localhost/healthdw20161216')
 
-def get_datasets(years):
+def get_datasets(test_id, years, engine):
     datasets = []
+    query = 'select results.*, tests.test_code from results' + \
+            ' inner join dates on results.service_date_id=dates.date_id' + \
+            ' inner join tests on tests.test_id=results.test_id' + \
+            ' where tests.test_id={} and dates.gregorian_year = {};'
     for year in years:
-        data = pd.read_sql(query % year, engine)
+        data = pd.read_sql(query.format(test_id, year), engine)
         data.rename(columns={'numeric_result': str(year)}, inplace=True)
         data = data.set_index('customer_id')
         datasets.append(data)
@@ -90,36 +93,63 @@ def predict_forward(df, txm, states, values, pctchange_col, nyears=1):
     lo_results = pd.DataFrame(lores, index=indexes)
     return hi_results, lo_results
 
-query = 'select results.*, tests.test_code from results' + \
-        ' inner join dates on results.service_date_id=dates.date_id' + \
-        ' inner join tests on tests.test_id=results.test_id' + \
-        ' where tests.test_id=68 and dates.gregorian_year = %d;'
-
-# years = [2011, 2012, 2013, 2014, 2015]
-
-# regcust = get_regular_customers(get_datasets(years), years)
-lowage = 25
-hiage = 55
-# flt_regcust = regcust[(regcust['age']>=lowage) & (regcust['age']<=hiage)]
-'''needs to iterate all years'''
-# flt_regcust = flt_regcust.assign(pctchange1 = (flt_regcust['2012'] - flt_regcust['2011'])*100/flt_regcust['2011'])
-states = np.arange(-50, 100, 10)
-states_labels = [str(s) for s in states][1:]
-
-# needs to iterate all pct changes
-# flt_regcust = flt_regcust.assign(pctchange1cat=pd.cut(flt_regcust['pctchange1'], states, right=True, labels=states_labels))
-pcts = ['pctchange1cat', 'pctchange2cat', 'pctchange3cat', 'pctchange4cat']
-# matrices = cal_transition_prob(flt_regcust, states_labels, pcts)
-# txm = pd.DataFrame(matrices, columns=states_labels, index=states_labels).T
-# output txm
-
 @click.command()
 @click.argument('test')
 @click.argument('years')
 @click.option('--db', help='database')
+@click.option('--username', help='database username')
 @click.option('--dbhost', help='database host', default='localhost')
-def main(test, years, db, dbhost):
-    print(test, years, db, dbhost)
+@click.option('--minage', help='minimum age', default='25', type=int)
+@click.option('--maxage', help='maximum age', default='55', type=int)
+def main(test, years, db, dbhost, username, minage, maxage):
+    print('Parameters:\n\tTest: {}, Years: {}, DB: {}, DBHost: {}'.format(test, years, db, dbhost))
+    if not db:
+        db = 'healthdw20161216'  # just for convenience, not used in production
+    engine = create_engine('postgresql+psycopg2://{}@{}/{}'.format(username, dbhost, db))
+    if not test or not years:
+        print('Some required parameters are missing.')
+        raise SystemExit
+    else:
+        try:
+            df = pd.read_sql('select * from tests', engine)
+        except Exception as e:
+            print('Cannot connect to the database.')
+            raise e
+            raise SystemExit
+        try:
+            test_id = df[df['test_code']==test.upper()].test_id.iloc[0]
+        except IndexError:
+            print('{} not found.'.format(test))
+
+
+        years = [int(year) for year in years.split(',')]
+        regcust = get_regular_customers(get_datasets(test_id, years, engine), years)
+        flt_regcust = regcust[(regcust['age']>=minage) & (regcust['age']<=maxage)]
+
+        for i in range(len(years)-1):
+            curyear = str(years[i])
+            nextyear = str(years[i+1])  # not need to be string in the first place
+            column = 'pctchange%d' % (i+1)
+            kwargs = {column: lambda x: (x[nextyear] - x[curyear])*100/x[curyear]}
+            flt_regcust = flt_regcust.assign(**kwargs)
+
+        states = np.arange(-50, 100, 10)
+        states_labels = [str(s) for s in states][1:]
+
+        # needs to iterate all pct changes
+        for colname in flt_regcust.columns:
+            if colname.startswith('pctchange'):
+                column = colname + 'cat'
+                kwargs = {
+                    column: lambda x: pd.cut(x[colname], states, right=True, labels=states_labels)
+                }
+                flt_regcust = flt_regcust.assign(**kwargs)
+
+        pcts = ['pctchange{}cat'.format(i+1) for i in range(len(years)-1)]
+        matrices = cal_transition_prob(flt_regcust, states_labels, pcts)
+        txm = pd.DataFrame(matrices, columns=states_labels, index=states_labels).T
+        txm.to_csv('{}_mat_{}-{}.csv'.format(test,years[0], years[-1]))
+
 
 
 if __name__ == '__main__':
